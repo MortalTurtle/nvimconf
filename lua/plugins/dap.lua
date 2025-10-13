2f041102436f0754b229aea59dce9a4e7f317cc8
Arsenyi Lukin <mortalturtle@qavm-b738e996.qemu>
2025-10-13 11:57:29 +0300

    Fixes

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -5,7 +5,7 @@ vim.keymap.set('n', '<F5>', dap.continue, {desc = "Continue"})
 vim.keymap.set('n', '<F10>', dap.step_over, {desc = "Step over"})
 vim.keymap.set('n', '<F11>', dap.step_into, {desc = "Step into"})
 vim.keymap.set('n', '<F12>', dap.step_out, {desc = "Steb out"})
-vim.keymap.set('n', '<leader>bp', dap.toggle_breakpoint, {desc = "Breakpoint"})
+vim.keymap.set('n', '<leader>bb', dap.toggle_breakpoint, {desc = "Breakpoint"})
 vim.keymap.set('n', '<leader>B', function() 
   dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) 
 end,
